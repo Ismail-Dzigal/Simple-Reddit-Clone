2eed2cdb877a9fa000d850dfdfdb9a26f8cbb6d2
Ismail-Dzigal <ismail_dzigal@yahoo.com>
2017-11-05 15:26:07 +0100

    Files added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  articles: Article[];
+  vidljiv:boolean = false;
+
+  constructor(){
+    this.articles = [
+      new Article('Angular 4', 'http://angular.io', 3),
+      new Article('Pluralsight', 'https://www.pluralsight.com', 2),
+      new Article('W3Schools', 'https://www.w3schools.com', 1)
+    ];
+  }
+
+  addArticle(title:HTMLInputElement, link:HTMLInputElement):boolean{
+    this.articles.push(new Article(title.value,link.value, 0));
+    title.value = '';
+    link.value = '';
+    this.vidljiv = !this.vidljiv;
+    return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
+
+  vidiSe():void {
+   this.vidljiv = !this.vidljiv;
+  }
+  
 }
